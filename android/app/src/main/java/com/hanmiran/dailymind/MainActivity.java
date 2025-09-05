4db67b62b22db7a50906303a8dc54a1c839092fc
hanmiran <hanmirann@nate.com>
2025-09-05 16:20:48 +0900

    Add Android project files for EAS build

--- android/app/src/main/java/com/hanmiran/dailymind/MainActivity.java
+++ android/app/src/main/java/com/hanmiran/dailymind/MainActivity.java
@@ -1,4 +1,4 @@
-package com.dailymind;
+package com.hanmiran.dailymind;
 
 import android.os.Build;
 import android.os.Bundle;
--- android/app/src/main/java/com/hanmiran/dailymind/MainApplication.java
+++ android/app/src/main/java/com/hanmiran/dailymind/MainApplication.java
@@ -1,4 +1,4 @@
-package com.dailymind;
+package com.hanmiran.dailymind;
 
 import android.app.Application;
 import android.content.res.Configuration;
